453645d11786fe3e9fd7325930d6b556ebeaeb99
Matt <bappy13@hotmail.com>
2014-07-22 15:15:30 -0400

    Implemented Shunting Yard

--- RPNInterpreter.py
+++ RPNInterpreter.py
@@ -1,24 +1,28 @@
+#RPNParser takes a list of tokens in Reverse Polish Notation and evaluates it
 def RPNParser(exp):
-    output_stack = []
+    #The output stack
+    stk = []
     while len(exp):
         x = exp.pop(0)
         
         if x == "+" or x == "-" or x == "*" or x == "/" or x == "^":
-            b = output_stack.pop()
-            a = output_stack.pop()
-            output_stack.append(operator_parse(a, b, x))
+            b = stk.pop()
+            a = stk.pop()
+            stk.append(operator_parse(a, b, x))
         else:
-            output_stack.append(x)
+            stk.append(x)
 
-    return output_stack[0]
+    return stk[0]
 
 
 
 def operator_parse(a, b, operator):
     
+    #convert the input from strings to floats
     a = float(a)
     b = float(b)
     
+    #perform the operation
     if operator == "+":
         return a + b
     elif operator == "-":
@@ -30,5 +34,7 @@ def operator_parse(a, b, operator):
     elif operator == "^":
         return a ** b
 
+#Can be used to test/see the Parser in action
+#exp = ['4', '6', '4', '2', '^', '7', '*', '-', '4', '^', '*']
 
-exp = ['1', '3', '+', '2', '^', '7', '+', '2', '-']
+#print(RPNParser(exp))
--- ShuntingYard.py
+++ ShuntingYard.py
@@ -1,6 +1,66 @@
 import RPNInterpreter
 
-RPNP = RPNINterpreter.RPNParser
+RPNP = RPNInterpreter.RPNParser
 
+def precedence_lookup(token):
+    if token == '+':
+        return (2, 'left')
+    elif token == '-':
+        return (2, 'left')
+    elif token == '*':
+        return (3, 'left')
+    elif token == '/':
+        return (3, 'left')
+    elif token == '^':
+        return (4, 'right')
+    else:
+        return "error"
+
+def shunting_yard(exp):
+    #output queue
+    q = []
+    #operator stack
+    stk = []
+    while(len(exp) > 0):
+        x = exp.pop(0)
+        if x == "+" or x == "-" or x == "*" or x == "/" or x == "^":
+            #determine precedence and associativity
+            ptoken = precedence_lookup(x)
+            if len(stk) > 0:
+                temp = precedence_lookup(stk[-1])
+            
+                while (len(stk) > 0 and ptoken[0] <= temp[0] and ptoken[1] == 'left') or (len(stk) > 0 and ptoken[0] < temp[0]):
+                    y = stk.pop()
+                    q.append(y)
+
+            stk.append(x)
+
+        elif x == '(':
+            stk.append(x)
+        elif x == ')':
+            while not stk[-1] == '(':
+                temp = stk.pop()
+                q.append(temp)
+            stk.pop()
+        else:
+            q.append(x)
+                
+    while len(stk) > 0:
+        q.append(stk.pop())
+    return q
+
+def string_parse(inp):
+    exp = []
+    for x in range(len(inp)):
+        if not inp[x] == ' ':
+            exp.append(inp[x])
+    return exp
+
+
+exp = raw_input("Please enter an expression to be evaluated: \n")
+exp = string_parse(exp)
+exp = shunting_yard(exp)
+result = RPNP(exp)
+print(result)
 
 
